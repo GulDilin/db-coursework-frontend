538114a3e8f7a3b3dbba3e135e893833050d787a
GulDilin <zhenyagurin@gmail.com>
2021-01-23 01:58:07 +0300

    Add plug page

---
+++ src/components/Plug.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card
+    dark
+    color="blue-grey"
+    class="ma-8"
+  >
+    <v-card-title>
+      {{ title }}
+    </v-card-title>
+    <v-card-text>
+      Not available now
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: "Plug",
+
+    props: {
+      title: {
+        type: String,
+        default: "Temporary"
+      }
+    }
+  }
+</script>
